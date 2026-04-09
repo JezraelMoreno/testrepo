af89e7e84d08c41e780bca72ef1a22b3df5fb34e
JezraelMoreno <a01785888@tec.mx>
2026-04-08 22:42:24 -0600

    Error en app.js

--- sample-app/app.js
+++ sample-app/app.js
@@ -4,7 +4,7 @@ const app = express();
 // app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello from sample app');
+    res.send('mello from sample app');
 });
 
 app.get("/name/:name", (req, res) => {
