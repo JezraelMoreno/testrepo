e7243a8c6ee30ce38de36fdf773c7fd61f2a5c37
JezraelMoreno <a01785888@tec.mx>
2026-04-08 22:41:26 -0600

    inital commit

---
+++ sample-app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+// app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello from sample app');
+});
+
+app.get("/name/:name", (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+    //res.render("hello", { name: req.params.name });
+});
+
+module.exports = app;
