5bdd8026504c66867384b32eb185aaadc267c401
JezraelMoreno <a01785888@tec.mx>
2026-04-08 22:43:03 -0600

    Error corregido

--- sample-app/app.test.js
+++ sample-app/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
     test('GET / should return hello from sample app', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello from sample app');
+        expect(response.text).toBe('Hello from sample app Hector');
     });
 });
